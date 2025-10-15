da694f9044d86535ae6574c4139596180ed4bd9f
kavyapalla14 <2400031781@kluniversity.in>
2025-10-15 17:02:33 +0530

    Feature 2 changes

--- App.jsx
+++ App.jsx
@@ -1,3 +1,11 @@
-function greet() {
+/*function greet() {
   console.log("Hello, World!");
 }
+function greet() {
+  console.log("Hello from Feature 1!");
+}*/
+function greet() {
+  console.log("Hello from Feature 2!");
+}
+
+
