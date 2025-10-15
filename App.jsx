016ffc1765c01969677bbe9dd26987e1cfee82a0
kavyapalla14 <2400031781@kluniversity.in>
2025-10-15 17:19:13 +0530

    Initial Commit

--- App.jsx
+++ App.jsx
@@ -3,9 +3,13 @@
 }
 function greet() {
   console.log("Hello from Feature 1!");
-}*/
+}
 function greet() {
   console.log("Hello from Feature 2!");
+}*/
+function greet() {
+  console.log("Hello from both Feature 1 and Feature 2!");
 }
+
 
 
